7c8faedf8369aab0a31cec50abec382a042805c4
Matis <matis.baguelin@gmail.com>
2023-12-01 15:45:28 +0100

    day 01 part 1 v2

--- day01/p1.js
+++ day01/p1.js
@@ -3,28 +3,14 @@ const fs = require("fs");
 const data = fs.readFileSync("./input.txt", "utf8");
 const lines = data.split("\n");
 
-let sum = 0;
-
-for (const line of lines) {
-  let calibration_value = "";
-
-  // get first number from line
-  for (const char of line) {
-    if (!isNaN(parseInt(char))) {
-      calibration_value = char;
-      break;
-    }
-  }
-
-  // get last number from line
-  for (let i = line.length - 1; i >= 0; i--) {
-    if (!isNaN(parseInt(line[i]))) {
-      calibration_value += line[i];
-      break;
-    }
-  }
-
-  sum += parseInt(calibration_value);
-}
+const sum = lines
+  .map((line) => {
+    const firstNumber = [...line].find((char) => !isNaN(parseInt(char)));
+    const lastNumber = [...line]
+      .reverse()
+      .find((char) => !isNaN(parseInt(char)));
+    return parseInt(firstNumber + lastNumber);
+  })
+  .reduce((a, b) => a + b, 0);
 
 console.log("part 1 :", sum);
